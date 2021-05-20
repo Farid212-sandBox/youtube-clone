76acb8e2d2964133a0ffe93ca355f80489898c2a
Farid <farid.hammouten@gmail.com>
2021-05-20 23:03:29 +0900

    init lateral nav menu component

---
+++ src/components/LateralNavBar/LateralNavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'LateralNavBar.css'
+
+const LateralNavBar = () =>{
+	return(
+		<div className="LateralNavBar">
+		</div>
+	)
+}
+
+export default LateralNavBar
--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './HomePage.css'
 
 import Header from '../../components/Header'
+import LateralNavBar from '../../components/LateralNavBar'
 
 const HomePage = () =>{
 	return (
 		<div className="HomePage">
 			<Header />
 
-			{/* LateralNavBar */}
+			<LateralNavBar />
 
 			{/* MainVideosBox */}
 		</div>

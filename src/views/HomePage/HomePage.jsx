836ba727f9d2d409c3d32685e324a52d2cea55bb
Farid <farid.hammouten@gmail.com>
2021-05-21 03:51:10 +0900

    updated

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 import './HomePage.css'
 
 import Header from '../../components/Header'
-import LateralNavBar from '../../components/LateralNavBar'
 
 const HomePage = () =>{
 	return (
 		<div className="HomePage">
 			<Header />
-
-			<LateralNavBar />
-
-			{/* MainVideosBox */}
+			{/* MainVideosBoxes */}
 		</div>
 	)
 }
